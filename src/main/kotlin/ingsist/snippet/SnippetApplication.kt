e900c1937c7e9dded0b81abfd60c8237bf6a9f64
ignaciogaspar <igaspar@mail.austral.edu.ar>
2025-10-20 23:32:24 -0300

    Inicializacion de proyecto snippet + CI

---
+++ src/main/kotlin/ingsist/snippet/SnippetApplication.kt
@@ -0,0 +1,11 @@
+package ingsist.snippet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetApplication>(*args)
+}
---
+++ src/test/kotlin/ingsist/snippet/SnippetApplicationTests.kt
@@ -0,0 +1,13 @@
+package ingsist.snippet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
