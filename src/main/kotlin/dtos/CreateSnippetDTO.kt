86c6d902b98d6f5c91d88ea31276fe2804e27c45
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-10-29 12:44:12 -0300

    feat/useCase#1: created upload snippet controller
    # Conflicts:
    #	build.gradle

---
+++ src/main/kotlin/dtos/CreateSnippetDTO.kt
@@ -0,0 +1,11 @@
+package dtos
+
+import jakarta.validation.constraints.NotBlank
+
+class CreateSnippetDTO(
+    @field:NotBlank val code: String,
+    @field:NotBlank val name: String,
+    @field:NotBlank val language: String,
+    @field:NotBlank val version: String,
+    @field:NotBlank val description: String,
+    )
