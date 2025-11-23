2333b35f507d8b1c2490d659c2ff08075e37bbbd
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-11-23 10:06:41 -0300

    snippet controller done, snippet service in process

--- src/main/kotlin/ingsist/snippet/controller/SnippetController.kt
+++ src/main/kotlin/ingsist/snippet/controller/SnippetController.kt
@@ -2,6 +2,7 @@ package ingsist.snippet.controller
 
 import ingsist.snippet.domain.snippet.SnippetUploadResult
 import ingsist.snippet.dtos.CreateSnippetDTO
+import ingsist.snippet.dtos.SnippetUploadDTO
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,29 +11,45 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import ingsist.snippet.service.SnippetService
+import jakarta.validation.Valid
 
 @RestController
 @RequestMapping("/snippets")
 class SnippetController(
     private val snippetService: SnippetService
 ) {
-    @PostMapping("/upload")
-    suspend fun uploadSnippet(
+    @PostMapping("/upload-from-file")
+    suspend fun uploadSnippetFromFile(
         @RequestParam("file") file: MultipartFile,
-        @RequestParam("name") name: String,
-        @RequestParam("language") language: String,
-        @RequestParam("version") version: String,
-        @RequestParam("description") description: String
+        @Valid params: SnippetUploadDTO
     ) : ResponseEntity<Any> {
         val code = file.bytes.toString(Charsets.UTF_8)
-        val snippet = CreateSnippetDTO(code, name, language, version, description)
+        return uploadSnippetLogic(code, params)
+    }
+
+    @PostMapping("/upload-inline")
+    suspend fun uploadSnippetInline(
+        @RequestParam("code") code: String,
+        @Valid params: SnippetUploadDTO
+    ) : ResponseEntity<Any> {
+        return uploadSnippetLogic(code, params)
+    }
+
+    private suspend fun uploadSnippetLogic(
+        code: String,
+        params: SnippetUploadDTO
+    ) : ResponseEntity<Any> {
+        val snippet = CreateSnippetDTO(code, params.name, params.language,
+            params.version, params.description)
         val result = snippetService.createSnippet(snippet)
+        return resultHandler(result)
+    }
+
+    private suspend fun resultHandler(result: SnippetUploadResult): ResponseEntity<Any> {
         return when (result) {
             is SnippetUploadResult.Success -> ResponseEntity.status(HttpStatus.CREATED).body(result)
             is SnippetUploadResult.InvalidSnippet -> ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(result)
             is SnippetUploadResult.UnsupportedLanguage -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result)
         }
     }
-
-
 }
