2d13c329ac6a503e0738020d18523ce025c1b077
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-11-26 14:44:09 -0300

    deleted old migrations

--- src/main/resources/db/migration/V1__create_snippets.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE snippets (
-                          id          VARCHAR(50) PRIMARY KEY,
-                          name        VARCHAR(255) NOT NULL,
-                          language    VARCHAR(50) NOT NULL,
-                          version     VARCHAR(20) NOT NULL,
-                          description TEXT,
-                          asset_key   VARCHAR(255) NOT NULL,
-                          created_at  TIMESTAMPTZ DEFAULT now()
-);
